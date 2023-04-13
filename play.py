4c8a0c694825ce2cd41de336f9176bcd8e32fc18
Felipe Martins <fbm2@cin.ufpe.br>
2023-04-13 22:07:18 +0000

    summary code

---
+++ play.py
@@ -0,0 +1,164 @@
+import gym
+import torch
+from ppo_continuous_action_isaacgym import ExtractObsWrapper, Agent
+from abc import ABC, abstractmethod
+from collections import namedtuple
+import numpy as np
+
+
+def random_ou(prev):
+    ou_theta = 0.1
+    ou_sigma = 0.15
+    noise = (
+        prev
+        - ou_theta * prev
+        + torch.normal(
+            0.0,
+            ou_sigma,
+            size=prev.size(),
+            device=prev.device,
+            requires_grad=False,
+        )
+    )
+    return noise.clamp(-1.0, 1.0)
+
+
+class Team(ABC):
+    def __init__(self, path=None, env_d=None):
+        pass
+
+    @abstractmethod
+    def __call__(self, act, obs):
+        pass
+
+
+class TeamZero(Team):
+    def __call__(self, act, obs):
+        act[:] *= 0
+
+
+class TeamOU(Team):
+    def __call__(self, act, obs):
+        act[:] = random_ou(act)
+
+
+class TeamAgent(Team):
+    def __init__(self, path, env_d):
+        self.agent = Agent(env_d).to('cuda:0')
+        self.agent.load_state_dict(torch.load(path))
+
+
+class TeamSA(TeamAgent):
+    def __call__(self, act, obs):
+        act[:] = random_ou(act)
+        act[:, 0, :] = self.agent.get_action_and_value(obs[:, 0, :])[0]
+
+
+class TeamCMA(TeamAgent):
+    def __call__(self, act, obs):
+        act[:] = self.agent.get_action_and_value(obs[:, 0, :])[0].view(-1, 3, 2)
+
+
+class TeamDMA(TeamAgent):
+    def __call__(self, act, obs):
+        act[:] = self.agent.get_action_and_value(obs)[0]
+
+
+def get_team(algo, path=None):
+    # create dummy env named tuple with single observation and action spaces
+    dummy_env = namedtuple(
+        'dummy_env', ['single_observation_space', 'single_action_space']
+    )
+
+    if algo == 'ppo-sa':
+        env_d = dummy_env(
+            single_observation_space=gym.spaces.Box(-np.inf, np.inf, (52,)),
+            single_action_space=gym.spaces.Box(-1.0, 1.0, (2,)),
+        )
+        return TeamSA(path, env_d)
+    elif algo == 'ppo-sa-x3':
+        env_d = dummy_env(
+            single_observation_space=gym.spaces.Box(-np.inf, np.inf, (52,)),
+            single_action_space=gym.spaces.Box(-1.0, 1.0, (2,)),
+        )
+        return TeamDMA(path, env_d)
+    elif algo == 'ppo-dma':
+        env_d = dummy_env(
+            single_observation_space=gym.spaces.Box(-np.inf, np.inf, (52,)),
+            single_action_space=gym.spaces.Box(-1.0, 1.0, (2,)),
+        )
+        return TeamDMA(path, env_d)
+    elif algo == 'ppo-cma':
+        env_d = dummy_env(
+            single_observation_space=gym.spaces.Box(-np.inf, np.inf, (52,)),
+            single_action_space=gym.spaces.Box(-1.0, 1.0, (6,)),
+        )
+        return TeamCMA(path, env_d)
+    elif algo == 'zero':
+        return TeamZero()
+    elif algo == 'ou':
+        return TeamOU()
+    else:
+        raise ValueError(f'Unknown algo: {algo}')
+
+
+BASELINE_TEAMS = {
+    'ppo-sa': {
+        '10': get_team('ppo-sa', 'base_nets/exp000_ppo-sa_10/agent.pt'),
+        '20': get_team('ppo-sa', 'base_nets/exp000_ppo-sa_20/agent.pt'),
+        '30': get_team('ppo-sa', 'base_nets/exp000_ppo-sa_30/agent.pt'),
+    },
+    'ppo-sa-x3': {
+        '10': get_team('ppo-sa-x3', 'base_nets/exp000_ppo-sa_10/agent.pt'),
+        '20': get_team('ppo-sa-x3', 'base_nets/exp000_ppo-sa_20/agent.pt'),
+        '30': get_team('ppo-sa-x3', 'base_nets/exp000_ppo-sa_30/agent.pt'),
+    },
+    'ppo-cma': {
+        '10': get_team('ppo-cma', 'base_nets/exp000_ppo-cma_10/agent.pt'),
+        '20': get_team('ppo-cma', 'base_nets/exp000_ppo-cma_20/agent.pt'),
+        '30': get_team('ppo-cma', 'base_nets/exp000_ppo-cma_30/agent.pt'),
+    },
+    'ppo-dma': {
+        '10': get_team('ppo-dma', 'base_nets/exp000_ppo-dma_10/agent.pt'),
+        '20': get_team('ppo-dma', 'base_nets/exp000_ppo-dma_20/agent.pt'),
+        '30': get_team('ppo-dma', 'base_nets/exp000_ppo-dma_30/agent.pt'),
+    },
+    'zero': {'00': get_team('zero')},
+    'ou': {'00': get_team('ou')},
+}
+
+
+def play_matches(envs, blue_team, yellow_team, n_matches, video_path=None):
+    envs.reset_buf[:] = 1
+    envs.reset_dones()
+    if video_path:
+        envs.is_vector_env = True
+        envs = gym.wrappers.RecordVideo(
+            envs,
+            video_path,
+            step_trigger=lambda step: step == 0,
+            video_length=400,
+            name_prefix="video.000"
+        )
+    envs = ExtractObsWrapper(envs)
+
+    ep_count = 0
+    rew_sum = 0
+    len_sum = 0
+    action_buf = torch.zeros(
+        (envs.num_envs,) + envs.action_space.shape, device=envs.device
+    )
+    obs = envs.reset()
+    while ep_count < n_matches:
+        blue_team(action_buf[:, 0], obs[:, 0])
+        # print(act)
+        yellow_team(action_buf[:, 1], obs[:, 1])
+        obs, rew, dones, info = envs.step(action_buf)
+
+        env_ids = dones[:1065].nonzero(as_tuple=False).squeeze(-1)
+        if len(env_ids):
+            ep_count += len(env_ids)
+            rew_sum += rew[env_ids, 0, 0, 0].sum().item()
+            len_sum += info['progress_buffer'][env_ids].sum().item()
+
+    return rew_sum / ep_count, len_sum / ep_count
--- test.py
+++ test.py
@@ -2,37 +2,101 @@ import gym
 import isaacgym  # noqa
 import isaacgymenvs
 import torch
-
 from envs.vss import VSS
-
 # env setup
 from hydra import compose, initialize
 from isaacgymenvs.utils.reformat import omegaconf_to_dict
+from play import play_matches, get_team
 
-from envs.wrappers import SingleAgent, CMA, DMA
+N_GAMES = 15000
+SEED = 10
+NET_PATH = "base_nets/exp000_ppo-sa_10/agent.pt"
+RUN_ID = "2k8d2fxm"
+ALGO = 'ppo-sa'
 
 with initialize(config_path="envs"):
     cfg = compose(config_name="vss")
 cfg = omegaconf_to_dict(cfg)
 
-cfg['env']['numEnvs'] = 3
-
+cfg['env']['numEnvs'] = 1065
 envs = VSS(
     cfg=cfg,
     rl_device="cuda:0",
     sim_device="cuda:0",
     graphics_device_id=0,
     headless=False,
-    virtual_screen_capture=False,
-    force_render=True,
+    virtual_screen_capture=True,
+    force_render=False,
+    # force_render=False if video_path else True,
+)
+envs.w_goal = 1.0
+envs.w_grad = 0.0
+envs.w_move = 0.0
+envs.w_energy = 0.0
+
+from play import BASELINE_TEAMS
+import wandb
+
+blue_team = get_team(ALGO, NET_PATH)
+run = wandb.init(
+    project='ppo-isaac-cleanrl',
+    monitor_gym=False,
+    id=RUN_ID,
+    resume=True,
 )
 
-# envs = SingleAgent(envs)
-# envs = CMA(envs)
-envs = DMA(envs)
-envs.reset()
-act = torch.ones((envs.num_envs,) + envs.action_space.shape, device=envs.device)
-act[..., 0] = -2.0
+total_score = 0
+total_len = 0
+total_count = 0
 
-while 1:
-    obs, rew, dones, info = envs.step(act)
+for team in BASELINE_TEAMS:
+    team_score = 0
+    team_len = 0
+    for seed in BASELINE_TEAMS[team]:
+        print(f"Playing {team} {seed}")
+        yellow_team = BASELINE_TEAMS[team][seed]
+        rews, lens = play_matches(envs, blue_team, yellow_team, N_GAMES, f"runs/{team}_{seed}")
+        team_score += rews
+        team_len += lens
+        total_score += rews
+        total_len += lens
+        total_count += 1
+        run.log({f"Validation/Media/{team}/{seed}": wandb.Video(f"runs/{team}_{seed}/video.000-step-0.mp4")})
+    run.summary[f"Validation/Score/{team}"] = team_score / len(BASELINE_TEAMS[team])
+    run.summary[f"Validation/Length/{team}"] = team_len / len(BASELINE_TEAMS[team])
+run.summary["Validation/Score Mean"] = total_score / total_count
+run.summary["Validation/Length Mean"] = total_len / total_count
+# run.summary.update()
+
+wandb.finish()
+############################
+if ALGO == "ppo-sa":
+    blue_team = get_team('ppo-sa-x3', NET_PATH)
+
+    run = wandb.init(
+        project='ppo-isaac-cleanrl',
+        monitor_gym=False,
+        name=f"exp000_ppo-sa-x3_{seed}",
+        group="exp000"
+    )
+    total_score = 0
+    total_len = 0
+    total_count = 0
+
+    for team in BASELINE_TEAMS:
+        team_score = 0
+        team_len = 0
+        for seed in BASELINE_TEAMS[team]:
+            print(f"Playing {team} {seed}")
+            yellow_team = BASELINE_TEAMS[team][seed]
+            rews, lens = play_matches(envs, blue_team, yellow_team, N_GAMES, f"runs/{team}_{seed}")
+            team_score += rews
+            team_len += lens
+            total_score += rews
+            total_len += lens
+            total_count += 1
+            run.log({f"Validation/Media/{team}/{seed}": wandb.Video(f"runs/{team}_{seed}/video.000-step-0.mp4")})
+        run.summary[f"Validation/Score/{team}"] = team_score / len(BASELINE_TEAMS[team])
+        run.summary[f"Validation/Length/{team}"] = team_len / len(BASELINE_TEAMS[team])
+    run.summary["Validation/Score Mean"] = total_score / total_count
+    run.summary["Validation/Length Mean"] = total_len / total_count
