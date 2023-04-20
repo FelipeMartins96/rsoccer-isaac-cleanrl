3ffae3d0bbfcd9d099a3e68bd95ece54b70c1ff5
Felipe Martins <fbm2@cin.ufpe.br>
2023-04-20 19:20:31 +0000

    exp001 no video

--- ppo_continuous_action_isaacgym.py
+++ ppo_continuous_action_isaacgym.py
@@ -109,6 +109,8 @@ def parse_args():
         help="the scale factor applied to the reward during training")
     parser.add_argument("--record-video-step-frequency", type=int, default=20000,
         help="the frequency at which to record the videos")
+    parser.add_argument("--test", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
+        help="Toggle test runs, save to test path.")
     args = parser.parse_args()
     args.batch_size = int(args.num_envs * args.num_steps)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)
@@ -170,6 +172,12 @@ class ExtractObsWrapper(gym.ObservationWrapper):
 if __name__ == "__main__":
     save_path = "/home/CIN/fbm2/runs"
     args = parse_args()
+    if args.test:
+        save_path = "runs"
+        args.wandb_project_name = "test-cleanrl-rsoccer"
+        args.track = True
+        args.capture_video = False
+        args.total_timesteps = 1000000
     run_name = f"{args.exp_name}_ppo-{args.env_id}_{args.seed}"
     if args.track:
         import wandb
@@ -363,81 +371,85 @@ if __name__ == "__main__":
 
     # Save Model
     torch.save(agent.state_dict(), f"{save_path}/{run_name}/{run_name}-agent.pt")
-    wandb.save(f"{save_path}/{run_name}/{run_name}-agent.pt")
-    # envs.close()
-    writer.close()
-    wandb_id = wandb.run.id
-    wandb.finish()
-    print("Training finished")
-    print("Running evaluation")
-    unwrapped_env.w_goal = 1.0
-    unwrapped_env.w_grad = 0.0
-    unwrapped_env.w_move = 0.0
-    unwrapped_env.w_energy = 0.0
-    from play import play_matches, get_team, BASELINE_TEAMS
-    blue_team = get_team(f'ppo-{args.env_id}', f"{save_path}/{run_name}/{run_name}-agent.pt") 
-    run = wandb.init(
-        project=args.wandb_project_name,
-        monitor_gym=False,
-        id=wandb_id,
-        resume=True,
-    )
-
-    total_score = 0
-    total_len = 0
-    total_count = 0
-
-    for team in BASELINE_TEAMS:
-        team_score = 0
-        team_len = 0
-        for seed in BASELINE_TEAMS[team]:
-            print(f"Playing {team} {seed}")
-            yellow_team = BASELINE_TEAMS[team][seed]
-            rews, lens = play_matches(unwrapped_env, blue_team, yellow_team, 10000, f"{save_path}/{run_name}/val_{team}_{seed}" if args.track else None)
-            team_score += rews
-            team_len += lens
-            total_score += rews
-            total_len += lens
-            total_count += 1
-            run.log({f"Validation/Media/{team}/{seed}": wandb.Video(f"{save_path}/{run_name}/val_{team}_{seed}/video.000-step-0.mp4")})
-        run.summary[f"Validation/Score/{team}"] = team_score / len(BASELINE_TEAMS[team])
-        run.summary[f"Validation/Length/{team}"] = team_len / len(BASELINE_TEAMS[team])
-    run.summary["Validation/Score Mean"] = total_score / total_count
-    run.summary["Validation/Length Mean"] = total_len / total_count
-
-    wandb.finish()
-    if args.env_id == "sa":
-        print("Running evaluation for sa-x3")
-
-        blue_team = get_team('ppo-sa-x3', f"{save_path}/{run_name}/{run_name}-agent.pt")
-        run_name = f"{args.exp_name}_ppo-{args.env_id}-x3_{args.seed}"
-        new_config = vars(args)
-        new_config["env_id"] = "sa-x3"
-        run = wandb.init(
-            project=args.wandb_project_name,
-            monitor_gym=False,
-            name=run_name,
-            group=args.exp_name,
-            config=new_config,
-        )
-        total_score = 0
-        total_len = 0
-        total_count = 0
-
-        for team in BASELINE_TEAMS:
-            team_score = 0
-            team_len = 0
-            for seed in BASELINE_TEAMS[team]:
-                print(f"Playing {team} {seed}")
-                yellow_team = BASELINE_TEAMS[team][seed]
-                rews, lens = play_matches(unwrapped_env, blue_team, yellow_team, 10000, f"{save_path}/{run_name}/val_{team}_{seed}" if args.track else None)
-                team_score += rews
-                team_len += lens
-                total_score += rews
-                total_len += lens
-                total_count += 1
-                run.log({f"Validation/Media/{team}/{seed}": wandb.Video(f"{save_path}/{run_name}/val_{team}_{seed}/video.000-step-0.mp4")})
-            run.summary[f"Validation/Score/{team}"] = team_score / len(BASELINE_TEAMS[team])
-            run.summary[f"Validation/Length/{team}"] = team_len / len(BASELINE_TEAMS[team])
-        run.summary["Validation/Score Mean"] = total_score / total_count
-        run.summary["Validation/Length Mean"] = total_len / total_count
+    if args.track:
+        with torch.no_grad():
+            wandb.save(f"{save_path}/{run_name}/{run_name}-agent.pt")
+            # envs.close()
+            writer.close()
+            wandb_id = wandb.run.id
+            wandb.finish()
+            print("Training finished")
+            print("Running evaluation")
+            unwrapped_env.w_goal = 1.0
+            unwrapped_env.w_grad = 0.0
+            unwrapped_env.w_move = 0.0
+            unwrapped_env.w_energy = 0.0
+            from play import play_matches, get_team, BASELINE_TEAMS
+            blue_team = get_team(f'ppo-{args.env_id}', f"{save_path}/{run_name}/{run_name}-agent.pt") 
+            run = wandb.init(
+                project=args.wandb_project_name,
+                monitor_gym=False,
+                id=wandb_id,
+                resume=True,
+            )
+        
+            total_score = 0
+            total_len = 0
+            total_count = 0
+        
+            for team in BASELINE_TEAMS:
+                team_score = 0
+                team_len = 0
+                for seed in BASELINE_TEAMS[team]:
+                    print(f"Playing {team} {seed}")
+                    yellow_team = BASELINE_TEAMS[team][seed]
+                    rews, lens = play_matches(unwrapped_env, blue_team, yellow_team, 10000, f"{save_path}/{run_name}/val_{team}_{seed}" if args.capture_video else None)
+                    team_score += rews
+                    team_len += lens
+                    total_score += rews
+                    total_len += lens
+                    total_count += 1
+                    if args.capture_video:
+                        run.log({f"Validation/Media/{team}/{seed}": wandb.Video(f"{save_path}/{run_name}/val_{team}_{seed}/video.000-step-0.mp4")})
+                run.summary[f"Validation/Score/{team}"] = team_score / len(BASELINE_TEAMS[team])
+                run.summary[f"Validation/Length/{team}"] = team_len / len(BASELINE_TEAMS[team])
+            run.summary["Validation/Score Mean"] = total_score / total_count
+            run.summary["Validation/Length Mean"] = total_len / total_count
+        
+            wandb.finish()
+            if args.env_id == "sa":
+                print("Running evaluation for sa-x3")
+        
+                blue_team = get_team('ppo-sa-x3', f"{save_path}/{run_name}/{run_name}-agent.pt")
+                run_name = f"{args.exp_name}_ppo-{args.env_id}-x3_{args.seed}"
+                new_config = vars(args)
+                new_config["env_id"] = "sa-x3"
+                run = wandb.init(
+                    project=args.wandb_project_name,
+                    monitor_gym=False,
+                    name=run_name,
+                    group=args.exp_name,
+                    config=new_config,
+                )
+                total_score = 0
+                total_len = 0
+                total_count = 0
+        
+                for team in BASELINE_TEAMS:
+                    team_score = 0
+                    team_len = 0
+                    for seed in BASELINE_TEAMS[team]:
+                        print(f"Playing {team} {seed}")
+                        yellow_team = BASELINE_TEAMS[team][seed]
+                        rews, lens = play_matches(unwrapped_env, blue_team, yellow_team, 10000, f"{save_path}/{run_name}/val_{team}_{seed}" if args.capture_video else None)
+                        team_score += rews
+                        team_len += lens
+                        total_score += rews
+                        total_len += lens
+                        total_count += 1
+                        if args.capture_video:
+                            run.log({f"Validation/Media/{team}/{seed}": wandb.Video(f"{save_path}/{run_name}/val_{team}_{seed}/video.000-step-0.mp4")})
+                    run.summary[f"Validation/Score/{team}"] = team_score / len(BASELINE_TEAMS[team])
+                    run.summary[f"Validation/Length/{team}"] = team_len / len(BASELINE_TEAMS[team])
+                run.summary["Validation/Score Mean"] = total_score / total_count
+                run.summary["Validation/Length Mean"] = total_len / total_count
--- run_slurm.sh
+++ run_slurm.sh
@@ -1,12 +1,12 @@
 case $1 in
-0) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id sa --seed 10 --track --capture-video;;
-3) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id cma --seed 10 --track --capture-video;;
-6) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id dma --seed 10 --track --capture-video;;
-1) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id sa --seed 20 --track --capture-video;;
-4) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id cma --seed 20 --track --capture-video;;
-7) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id dma --seed 20 --track --capture-video;;
-2) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id sa --seed 30 --track --capture-video;;
-5) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id cma --seed 30 --track --capture-video;;
-8) python ppo_continuous_action_isaacgym.py --exp-name exp002 --env-id dma --seed 30 --track --capture-video;;
+0) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 10 --env-id sa  --track;;
+1) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 10 --env-id cma --track;;
+2) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 10 --env-id dma --track;;
+3) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 20 --env-id sa  --track;;
+4) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 20 --env-id cma --track;;
+5) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 20 --env-id dma --track;;
+6) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 30 --env-id sa  --track;;
+7) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 30 --env-id cma --track;;
+8) python ppo_continuous_action_isaacgym.py --exp-name exp002 --seed 30 --env-id dma --track;;
 *) echo "Opcao Invalida!" ;;
 esac
