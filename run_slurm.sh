e1de00aac7f2f74a7f8304040232419711361a9d
Felipe Martins <felipemartins96@gmail.com>
2023-05-04 13:27:16 -0300

    experiments goto

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,12 +1,11 @@
 case $1 in
-0) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 10 --env-id sa  --track;;
-1) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 10 --env-id cma --track;;
-2) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 10 --env-id dma --track;;
-3) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 20 --env-id sa  --track;;
-4) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 20 --env-id cma --track;;
-5) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 20 --env-id dma --track;;
-6) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 30 --env-id sa  --track;;
-7) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 30 --env-id cma --track;;
-8) python ppo_continuous_action_isaacgym.py --exp-name exp005 --seed 30 --env-id dma --track;;
+0) ppo_continuous_action_isaacgym.py --exp-name exp000 -seed 10 --track ;;
+1) ppo_continuous_action_isaacgym.py --exp-name exp001 -seed 10 --track --check-angle ;;
+2) ppo_continuous_action_isaacgym.py --exp-name exp002 -seed 10 --track --check-speed ;;
+3) ppo_continuous_action_isaacgym.py --exp-name exp003 -seed 10 --track --check-angle --check-speed ;;
+4) ppo_continuous_action_isaacgym.py --exp-name exp004 -seed 10 --track --terminal-rw ;;
+5) ppo_continuous_action_isaacgym.py --exp-name exp005 -seed 10 --track --terminal-rw --check-angle ;;
+6) ppo_continuous_action_isaacgym.py --exp-name exp006 -seed 10 --track --terminal-rw --check-speed ;;
+7) ppo_continuous_action_isaacgym.py --exp-name exp007 -seed 10 --track --terminal-rw --check-angle --check-speed ;;
 *) echo "Opcao Invalida!" ;;
 esac
