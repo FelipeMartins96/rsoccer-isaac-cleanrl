bb6bdbc3c2346a0230d11af5f3816fba6a539833
Felipe Martins <fbm2@cin.ufpe.br>
2023-06-16 00:33:06 -0300

    validate all hp

--- ppo_continuous_action_isaacgym.py
+++ ppo_continuous_action_isaacgym.py
@@ -71,9 +71,9 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="sa",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=500000000,
+    parser.add_argument("--total-timesteps", type=int, default=1000000000,
         help="total timesteps of the experiments")
-    parser.add_argument("--learning-rate", type=float, default=0.001,
+    parser.add_argument("--learning-rate", type=float, default=0.00085,
         help="the learning rate of the optimizer")
     parser.add_argument("--num-envs", type=int, default=3600,
         help="the number of parallel game environments")
@@ -87,9 +87,9 @@ def parse_args():
         help="the discount factor gamma")
     parser.add_argument("--gae-lambda", type=float, default=0.95,
         help="the lambda for the general advantage estimation")
-    parser.add_argument("--num-minibatches", type=int, default=4,
+    parser.add_argument("--num-minibatches", type=int, default=3,
         help="the number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=8,
+    parser.add_argument("--update-epochs", type=int, default=6,
         help="the K epochs to update the policy")
     parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles advantages normalization")
