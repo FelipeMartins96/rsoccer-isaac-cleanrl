5aa632047aacce1e36d970900a1e0f63c1c02666
Felipe Martins <felipemartins96@gmail.com>
2023-06-19 09:48:01 -0300

    exp035

--- ppo_continuous_action_isaacgym.py
+++ ppo_continuous_action_isaacgym.py
@@ -77,7 +77,7 @@ def parse_args():
         help="the learning rate of the optimizer")
     parser.add_argument("--num-envs", type=int, default=3600,
         help="the number of parallel game environments")
-    parser.add_argument("--num-steps", type=int, default=300,
+    parser.add_argument("--num-steps", type=int, default=400,
         help="the number of steps to run in each environment per policy rollout")
     parser.add_argument("--anneal-lr", type=lambda x: bool(strtobool(x)), default=False, nargs="?", const=True,
         help="Toggle learning rate annealing for policy and value networks")
@@ -89,7 +89,7 @@ def parse_args():
         help="the lambda for the general advantage estimation")
     parser.add_argument("--num-minibatches", type=int, default=4,
         help="the number of mini-batches")
-    parser.add_argument("--update-epochs", type=int, default=6,
+    parser.add_argument("--update-epochs", type=int, default=8,
         help="the K epochs to update the policy")
     parser.add_argument("--norm-adv", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True,
         help="Toggles advantages normalization")
