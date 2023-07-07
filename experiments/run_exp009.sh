8a75693cef8fa88e8420212d5fb4bafd4f18760c
Felipe Martins <felipemartins96@gmail.com>
2023-07-07 13:05:17 -0300

    add enemy policy ro vf-coef tests

--- experiments/run_exp009.sh
+++ experiments/run_exp009.sh
@@ -1,6 +1,6 @@
 EXP_NAME="exp009"
 EXP_NOTES="vf-coef 2"
-EXP_ARGS="--vf-coef 2"
+EXP_ARGS="--enemy-policy  --vf-coef 2"
 case $1 in
 0) xvfb-run -a python ppo_continuous_action_isaacgym.py --exp-name $EXP_NAME --wandb-notes="$EXP_NOTES" $EXP_ARGS --env-id dma --seed 10 --track --capture-video;;
 1) xvfb-run -a python ppo_continuous_action_isaacgym.py --exp-name $EXP_NAME --wandb-notes="$EXP_NOTES" $EXP_ARGS --env-id dma --seed 20 --track --capture-video;;
